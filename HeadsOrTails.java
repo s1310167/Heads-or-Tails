1e2bbd944d3e7ee61e0b43e47c253359d710b8d4
s1310167 <s1310167@u-aizu.ac.jp>
2024-07-04 20:00:17 +0900

    User name added

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -1,13 +1,23 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class HeadsOrTails{
 	int heads=0,tails=0;
+	String name;
 
 	public static void main(String[] args){
 		HeadsOrTails game = new HeadsOrTails();
+		game.userName();
 		game.coinToss();
 	}
 	
+	public void userName(){
+		System.out.println("Who are you?");
+		Scanner sc = new Scanner(System.in);
+		name = sc.nextLine();
+		System.out.println("Hello, " + name + "!");
+		
+	}
 	public void coinToss(){
 		System.out.println("Tossing a coin...");
 		
@@ -24,5 +34,4 @@ class HeadsOrTails{
 		}
 		
 		System.out.printf("Heads: %d, Tails: %d\n",heads,tails);
-	}
 }
